4adbe129c7bc066f1decf119e0b9b7f413fc7288
araaaaan <laran89@naver.com>
2023-09-11 17:52:04 +0200

    concept

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaaaaran <aaaaaran@student.42.fr>          +#+  +:+       +#+        */
+/*   By: arlee <arlee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 18:40:03 by arlee             #+#    #+#             */
-/*   Updated: 2023/09/11 19:46:46 by aaaaaran         ###   ########.fr       */
+/*   Updated: 2023/09/11 17:51:09 by arlee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,42 +18,51 @@
 
 // char *get_next_line(int fd)
 // {
-		
 // }
+#include <string.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-# define BUFFER_SIZE 32
+#define BUFFER_SIZE 32
 
-int main(void)
+int	main(void)
 {
-	int fd;
+	int	fd;
 	int	size;
-	char buff[5];
-	char *save;
+	char	buff[3000];
+	static char	*save;
 
-	if ((fd = open("test.txt", O_RDONLY)) == -1)
+	if ((fd = open("test.txt", O_RDONLY)) == -1) // error : 1
 	{
 		return (0);
 	}
-	
-	
-	
-	while (( size = read(fd, buff, 5)) != 0) // read 5bytes each and write (no, size = ~ seperatly once)
-	{
-		if (size == -1)
+
+	// while (( size = read(fd, buff, 5)) != 0) // read 5bytes each and write (no, size = ~ seperatly once)
+	// {
+	// 	if (size == -1) // 0: successful completion/ >0 end-of-file or Error
+	// 	{
+	// 		return (0);
+	// 	}
+	// 	write(1, buff, size);
+	// }
+	size =  read(fd, buff, 100); // read (n)bytes and save in buff
+	printf("size : %d\n", size);
+	if(fd > 0 && strchr(buff, '\n'))
 		{
-			return (0);
+			save = strchr(buff, '\n');
+			printf("found \n");
+			printf("contents: %s", buff);
+			printf("%s", save );
 		}
-		write(1, buff, size);
-	}
+	//write(1, buff, size);	
 	close(fd);
+	return 0;
 }
 
 /*
 1. check fd. check string
 2. (open file), read file read(int fd, void *buff, size_t byte)
-3. if ft_strchr(string, /n) : save in the static char *  variable?
+3. if ft_strchr(string, /n) : save in the static char *  variable? (save and save and save...)
 4. 
 
 read, malloc, free
